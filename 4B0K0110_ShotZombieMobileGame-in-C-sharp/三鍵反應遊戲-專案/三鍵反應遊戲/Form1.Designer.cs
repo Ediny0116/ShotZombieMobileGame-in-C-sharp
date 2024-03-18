d82e05384c16c7ef96cf9e12fe9bf04431d229a0
Ediny0116 <87802953+Ediny0116@users.noreply.github.com>
2024-03-18 21:56:42 +0800

    Add files via upload

---
+++ 4B0K0110_ShotZombieMobileGame-in-C-sharp/三鍵反應遊戲-專案/三鍵反應遊戲/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace 三鍵反應遊戲
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timeDelay = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timerGame = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.countDown = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(12, 18);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 210);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "XXXXXXXXXXXXXXXXXXXX";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 10F);
+            this.button1.Location = new System.Drawing.Point(176, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "玩法";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(120, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "時間";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("微軟正黑體", 16F);
+            this.button4.Location = new System.Drawing.Point(32, 270);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 40);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "開始";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timeDelay
+            // 
+            this.timeDelay.Tick += new System.EventHandler(this.timeDelay_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 16F);
+            this.label2.Location = new System.Drawing.Point(120, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 28);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "紀錄";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(14, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 27);
+            this.label3.TabIndex = 7;
+            this.label3.Text = " １ ２ ３";
+            // 
+            // timerGame
+            // 
+            this.timerGame.Interval = 10;
+            this.timerGame.Tick += new System.EventHandler(this.timerGame_Tick);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button5.Location = new System.Drawing.Point(118, 270);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 40);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "結束";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(165, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 21);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "0.0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 109);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(90, 120);
+            this.textBox2.TabIndex = 11;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "問號.jpeg");
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "遊戲畫面";
+            // 
+            // countDown
+            // 
+            this.countDown.Interval = 1000;
+            this.countDown.Tick += new System.EventHandler(this.countDown_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(216, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 12);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "新";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(216, 129);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 12);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "↑";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(216, 141);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 12);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "↓";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(216, 158);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(17, 12);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "舊";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 331);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "三鍵反應遊戲";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timeDelay;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timerGame;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer countDown;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ 4B0K0110_ShotZombieMobileGame-in-C-sharp/三鍵反應遊戲-專案/三鍵反應遊戲/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 三鍵反應遊戲
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //變數                   
+        public string txt, txtA = "O==", txtB = "=O=", txtC = "==O", txtE = "XXXXXXXXXXXXXXXXXXXX";
+
+        public int txtL, tag, cnt;
+
+        public Random rnd = new Random();
+
+        double DScnt, DTcnt = 0, CCnt;
+
+        public bool miss;
+
+        //教學
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "開始遊戲：按下鍵盤Enter或｢開始｣按鈕\r\n" +
+                "數字鍵123：打擊遊戲畫面上的對應位置\r\n\r\n" +
+                "遊戲畫面上會出現30個目標｢O｣\r\n" +
+                "使用數字鍵123來消除對應目標\r\n" +
+                "若打錯目標將會被禁止打擊0.5秒\r\n\r\n" +
+                "嘗試在最短時間內消除所有目標！"
+                , "玩法", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        //鍵盤輸入
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                button4.PerformClick();
+            }
+
+            if (miss == false)
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.NumPad1:
+                        gameLogic(3);
+                        break;
+                    case Keys.NumPad2:
+                        gameLogic(2);
+                        break;
+                    case Keys.NumPad3:
+                        gameLogic(1);
+                        break;
+                }
+            }
+            else
+                return;
+        }
+
+
+        //避免聚焦textbox
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            button4.Focus();
+        }
+
+        //開始按鈕
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = txtE;
+            button1.Enabled = false;
+            button4.Enabled = false;
+            CCnt = 0;
+            countDown.Enabled = true;
+            label3.Text = " 倒數3";
+        }
+
+        //倒數計時
+        private void countDown_Tick(object sender, EventArgs e)
+        {
+
+            miss = true;
+            CCnt++;
+
+            switch (CCnt)
+            {
+                case 1:
+                    label3.Text = " 倒數2";
+                    break;
+                case 2:
+                    label3.Text = " 倒數1";
+                    start();
+                    break;
+                case 3:
+                    label3.Text = " １ ２ ３";
+                    countDown.Enabled = false;
+                    miss = false;
+                    timerGame.Enabled = true;
+                    button5.Enabled = true;
+                    button5.Focus();
+                    break;
+            }
+        }
+
+        //開始按鈕
+        private void start()
+        {
+            textBox1.Enabled = true;
+            textBox1.Text = Convert.ToString(txtE);
+
+            //生成打擊目標
+            for (cnt = 1; cnt <= 30; cnt++)
+            {
+                int tag = rnd.Next(1, 4);
+                switch (tag)
+                {
+                    case 1:
+                        textBox1.Text = textBox1.Text + "\r\n" + Convert.ToString(txtA);
+                        break;
+                    case 2:
+                        textBox1.Text = textBox1.Text + "\r\n" + Convert.ToString(txtB);
+                        break;
+                    case 3:
+                        textBox1.Text = textBox1.Text + "\r\n" + Convert.ToString(txtC);
+                        break;
+                }
+            }
+
+            txtL = textBox1.Text.Length;
+
+            //將畫面移至最下方
+            this.textBox1.Select(txtL, 0);                                               //選取到最後一個字元
+            this.textBox1.ScrollToCaret();                                               //捲動至選取的位置
+
+
+            stT = DateTime.Now.TimeOfDay;
+            //button4.Enabled = false;
+
+        }
+
+
+        //結束按鈕
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timerGame.Enabled = false;
+            button4.Enabled = true;
+            button4.Focus();
+            button1.Enabled = true;
+            button5.Enabled = false;
+        }
+
+        //懲罰計時
+        private void timeDelay_Tick(object sender, EventArgs e)
+        {
+            //button4.Enabled = false;
+            txtL = textBox1.Text.Length;
+
+            DTcnt++;
+
+            double BT = 10 - DTcnt;
+
+            DScnt = BT / 10;
+
+            label3.Text = "懲罰" + Convert.ToString(DScnt);
+
+
+            if (DTcnt >= 5)
+            {
+                timeDelay.Enabled = false;
+                label3.Text = " １ ２ ３";
+                miss = false;
+            }
+
+        }
+
+        //時間計時
+        TimeSpan ST, nT, stT;
+        private void timerGame_Tick(object sender, EventArgs e)
+        {
+            nT = DateTime.Now.TimeOfDay;
+
+            ST = nT - stT;
+
+            double S = Math.Round((double)Convert.ToDouble(ST.TotalMilliseconds) / 1000, 2);
+
+            label4.Text = Convert.ToString(S) + "s";
+
+            if (textBox1.Text.Length == 20)
+            {
+                timerGame.Enabled = false;
+                textBox2.Text = Convert.ToString(S)+"s" + "\r\n"+textBox2.Text;
+                button4.Enabled = true;
+                button4.Focus();
+                button1.Enabled = true;
+                button5.Enabled = false;
+            }
+
+        }
+
+        //目標判斷
+        public void gameLogic(int pos)
+        {
+            txtL = textBox1.Text.Length;
+            txt = textBox1.Text;
+
+            switch (txt.Substring(txtL - pos, 1))
+            {
+                case "O":
+                    miss = false;
+                    textBox1.Text = txt.Substring(0, txtL - 5);
+                    txtL = textBox1.Text.Length;
+                    this.textBox1.Select(txtL, 0);
+                    this.textBox1.ScrollToCaret();
+                    break;
+                case "=":
+                    miss = true;
+                    DTcnt = 0;
+                    timeDelay.Enabled = true;
+                    break;
+            }
+
+        }
+
+    }
+}
